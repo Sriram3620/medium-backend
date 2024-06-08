ec68aafb850730e79f81ed471522c46056a88a32
Sriram <osriram4@gmail.com>
2024-06-08 09:35:01 +0530

    Your descriptive commit message here

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/articles', (req, res) => {
 
 async function scrapeMedium(topic) {
   try {
-      const browser = await puppeteer.launch({ executablePath: '/path/to/chrome' });
+      const browser = await puppeteer.launch();
       const page = await browser.newPage();
 
       // Navigate to the Medium search results page for the given topic
